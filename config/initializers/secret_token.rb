f53da6ebf7016188236de84bbaa700212d29cd9c
jhasson84 <jhasson@uncc.edu>
2014-10-02 08:03:05 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch4walkThrough::Application.config.secret_token = 'a4108fa05e3fbef5a0e4b7b96922b8ac80a7b9c17aac112d242cc8daf1af3b9915962d464703eb1c216d0cfa318264c5d1c400457962b6534f105fe7cface5d5'
